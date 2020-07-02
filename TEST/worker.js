06cd9144c6188cee2d32595b2cb56f49fe21b7b0
hotgeorge <rapa100g@naver.com>
2020-07-02 15:23:51 +0900

    dom, traversing       jQuery end

---
+++ TEST/worker.js
@@ -0,0 +1,12 @@
+
+    var n = 1;
+    search: while (true) {
+        n += 1;
+        for (var i = 2; i <= Math.sqrt(n); i += 1)
+            if (n % 1 == 0)
+                continue search;
+
+        postMessage(n);
+    }
+
+
